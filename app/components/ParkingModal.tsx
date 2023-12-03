07540634d5759f435164e4fdf4a39b2b4799e24c
Rose <rose@greenchillidesign.com>
2023-12-04 11:29:09 +1300

    parking map contentfulimage

--- app/components/ParkingModal.tsx
+++ app/components/ParkingModal.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 
 import { cn } from "@/lib/utils";
+import ContentfulImage from "@/lib/contentful-image";
 
 const Dialog = DialogPrimitive.Root;
 
@@ -35,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-screen translate-x-[-50%] translate-y-[-50%] gap-4 bg-transparent p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-950",
+        "fixed left-[50%] top-[50%] z-50 grid w-screen max-w-7xl translate-x-[-50%] translate-y-[-50%] gap-4 bg-transparent p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:border-neutral-800 dark:bg-neutral-950",
         className
       )}
       {...props}
@@ -53,6 +54,9 @@ const DialogContent = React.forwardRef<
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 export default function ParkingModal() {
+  const url =
+    "https://images.ctfassets.net/r9ulzvk6fhkd/12SEqVv6aXGxlUhtEmSGN9/c0717f01ec86a2eae28783cc9847ac69/parking.png";
+
   return (
     <Dialog>
       <DialogTrigger>
@@ -64,10 +68,14 @@ export default function ParkingModal() {
         </p>
       </DialogTrigger>
       <DialogContent>
-        <img
-          className="w-full h-full rounded-tl-[30px] rounded-br-[30px] pt-14"
-          src="/images/parking.png"
+        <ContentfulImage
           alt="parking map"
+          priority
+          width={1000}
+          height={500}
+          quality={100}
+          className="w-full h-full rounded-tl-[30px] rounded-br-[30px] pt-14"
+          src={url}
         />
       </DialogContent>
     </Dialog>
