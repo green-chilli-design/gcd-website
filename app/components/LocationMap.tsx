3e6396979a028a95658aa8022ed465c375fb52d1
Rose <rose@greenchillidesign.com>
2023-12-05 18:01:13 +1300

    map customisation

--- app/components/LocationMap.tsx
+++ app/components/LocationMap.tsx
@@ -4,8 +4,8 @@ import React, { useEffect } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
 
 const center = {
-  lat: -36.849530258746896,
-  lng: 174.75714348593007,
+  lat: -36.84942242650752,
+  lng: 174.75708412460006,
 };
 
 export default function LocationMap() {
@@ -17,10 +17,11 @@ export default function LocationMap() {
         apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
         version: "weekly",
       });
-      const { Map } = await loader.importLibrary("maps");
-      const { AdvancedMarkerElement } = (await google.maps.importLibrary(
-        "marker"
-      )) as google.maps.MarkerLibrary;
+      const { Map, InfoWindow } = await loader.importLibrary("maps");
+      const { AdvancedMarkerElement, PinElement } =
+        (await google.maps.importLibrary(
+          "marker"
+        )) as google.maps.MarkerLibrary;
       const mapOptions: google.maps.MapOptions = {
         center,
         zoom: 18,
@@ -28,15 +29,33 @@ export default function LocationMap() {
       };
 
       const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+      const infoWindow = new InfoWindow();
 
-      // custom marker
-      const greenCircle = document.createElement("div");
-      greenCircle.className = "bg-green rounded-full w-5 h-5";
+      // Customise the pin
+      const greenCircle = document.createElement("span");
+      greenCircle.className = "material-symbols-outlined icon-24 text-green";
+      greenCircle.innerText = "circle";
+      const customPin = new PinElement({
+        background: "transparent",
+        borderColor: "transparent",
+        glyph: greenCircle,
+      });
 
       const marker = new AdvancedMarkerElement({
         position: center,
         map,
-        content: greenCircle,
+        content: customPin.element,
+        title:
+          "WHATEVER Building 2.4/18 Sale Street, Auckland CBD, Auckland 1010, New Zealand",
+      });
+
+      const infoWindowOptions: google.maps.InfoWindowOptions = {};
+
+      marker.addListener("click", () => {
+        console.log("ran");
+        infoWindow.close();
+        infoWindow.setContent(marker.title);
+        infoWindow.open(marker.map, marker);
       });
     };
 
