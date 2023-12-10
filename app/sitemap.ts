75bc49ebdd5a8dee7f4ca370aaf0fd88c561c597
Rose <rose@greenchillidesign.com>
2023-12-11 12:56:41 +1300

    dynamic sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,15 @@
+import { getAllCaseStudies, getAllPosts, getAllServices } from "@/lib/api";
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://gcd.nz";
-  return [
+
+  const staticPages = [
     {
       url: baseUrl,
       lastModified: new Date(),
     },
+    // TODO: Disabled for MVP
     // {
     //   url: baseUrl + "/services",
     //   lastModified: new Date(),
@@ -15,6 +18,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: baseUrl + "/case-studies",
       lastModified: new Date(),
     },
+    // TODO: Disabled for MVP
     // {
     //   url: baseUrl + "/blog",
     //   lastModified: new Date(),
@@ -32,4 +36,44 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
     },
   ];
+
+  const dynamicPages = [];
+
+  try {
+    // Fetch all case study pages
+    const caseStudies = await getAllCaseStudies(false);
+    const caseStudyPages = caseStudies.map((caseStudy) => {
+      return {
+        url: `${baseUrl}/case-studies/${caseStudy.slug}`,
+        lastModified: new Date(),
+      };
+    });
+    dynamicPages.push(...caseStudyPages);
+
+    // TODO: Disabled for MVP
+    // Fetch all service pages
+    // const services = await getAllServices(false);
+    // const servicePages = services.map((service) => {
+    //   return {
+    //     url: `${baseUrl}/services/${service.slug}`,
+    //     lastModified: new Date(),
+    //   };
+    // }    );
+    // dynamicPages.push(...servicePages);
+
+    // Fetch all blog post pages
+    // const posts = await getAllPosts(false);
+    // const postPages = posts.map((post) => {
+    //   return {
+    //     url: `${baseUrl}/posts/${post.slug}`,
+    //     lastModified: new Date(),
+    //   };
+    // }
+    // );
+    // dynamicPages.push(...postPages);
+  } catch (error) {
+    console.log(error);
+  }
+
+  return [...staticPages, ...dynamicPages];
 }
