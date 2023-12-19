54bfac5251a40c36e2d81b06604bffd2447a9cb8
Rose <rose@greenchillidesign.com>
2023-12-19 17:36:46 +1300

    parking modal mods

--- app/components/ParkingModal.tsx
+++ app/components/ParkingModal.tsx
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "dark:border-neutral-800 dark:bg-neutral-950 fixed left-[50%] top-[50%] z-50 grid w-screen max-w-7xl translate-x-[-50%] translate-y-[-50%] gap-4 bg-transparent p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        "dark:border-neutral-800 dark:bg-neutral-950 fixed left-[50%] top-[50%] z-50 grid w-screen max-w-[1920px] translate-x-[-50%] translate-y-[-50%] gap-4 bg-transparent p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
         className,
       )}
       {...props}
@@ -67,7 +67,7 @@ export default function ParkingModal() {
           View Parking Instructions
         </p>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="py-0 pt-6 lg:py-6">
         <ContentfulImage
           alt="parking map"
           priority
