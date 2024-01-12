a9495c3efb7cc7e7d92cedc34cde4b336338640d
Rose <rose@greenchillidesign.com>
2024-01-12 16:05:19 +1300

    discovery process content

--- app/components/DiscoveryProcess.tsx
+++ app/components/DiscoveryProcess.tsx
@@ -16,31 +16,31 @@ export default function DiscoveryProcess() {
     },
     {
       title: "02. User flow & process logic",
-      description: "TO DO.",
+      description: "Based on the information gathered in the previous step, we'll create a user flow and process logic that outlines the steps users will take to achieve their goals within the app..",
     },
     {
       title: "03. UX workshops with internal stakeholders",
-      description: "TO DO.",
+      description: "We'll conduct workshops with your internal stakeholders to gather more information about your users' needs and how they interact with your products or services.",
     },
     {
       title: "04. Wireframes gradually evolving to hi-res prototype(s)",
-      description: "TO DO.",
+      description: "Ahead of UX workshops we'll create wireframes and later hi-res prototypes of the app, which will be used to test the content and usability of each page together with the user flow and process logic.",
     },
     {
       title: "05. Customer workshops",
-      description: "TO DO.",
+      description: "Depending on the project, we may conduct customer workshops to gather feedback on the wireframes and prototype as we did in stages 3 & 4 with stakeholders.",
     },
     {
       title: "06. Technical specifications",
-      description: "TO DO.",
+      description: "Through stages 1 - 5 we'll create a technical specification document that outlines the technical requirements for the project, including the technology stack, any third-party integrations that will be required and key functionality.",
     },
     {
       title: "07. Final design",
-      description: "TO DO.",
+      description: "Once the hi-res prototypes have been workshopped and approved we'll work with you to finalise the design of the app, including the user interface and overall look and feel.",
     },
     {
       title: "08. Project costs and timeline",
-      description: "TO DO.",
+      description: "After completing stages 1-6, we'll provide a detailed project cost and timeline estimate, incorporating necessary contingencies. The GC Digital Discovery Process is a flexible and collaborative approach to understanding your business and user requirements, ensuring stakeholder alignment before building the solution.",
     },
   ];
 
