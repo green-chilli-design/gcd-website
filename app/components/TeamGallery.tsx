666f83a365600a9fac81428b6b8dfade7232a47a
Rose <rose@greenchillidesign.com>
2024-01-17 11:24:23 +1300

    responsive team gallery

--- app/components/TeamGallery.tsx
+++ app/components/TeamGallery.tsx
@@ -1,36 +1,66 @@
 "use client";
 
 import ContentfulImage from "@/lib/contentful-image";
+import { useEffect, useRef } from "react";
 import { register } from "swiper/element/bundle";
+import { SwiperOptions } from "swiper/types";
 register();
 
 export default function TeamGallery({ gcdTeam }: { gcdTeam: any }) {
+  const swiperEl = document.querySelector("swiper-container");
+
+  // swiper parameters
+  const swiperParams: SwiperOptions = {
+    slidesPerView: 2,
+    breakpoints: {
+      640: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 5,
+      },
+      1920: {
+        slidesPerView: 6,
+      },
+    },
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      pauseOnMouseEnter: true,
+    },
+  };
+
+  useEffect(() => {
+    if (swiperEl) {
+      Object.assign(swiperEl, swiperParams);
+      swiperEl.initialize();
+    }
+  }, [swiperEl]);
+
   return (
-    <swiper-container
-      slides-per-view="4"
-      speed="500"
-      loop="true"
-      autoplay="true"
-      autoplay-delay="2000"
-      autoplay-pause-on-mouse-enter="true"
-    >
-      {gcdTeam?.map((teamMember: any) => (
-        <swiper-slide
-          key={teamMember.firstName + teamMember.lastName}
-          class="h-[300px] w-[300px]"
-        >
-          {teamMember.actionShot && (
-            <ContentfulImage
-              priority
-              src={teamMember.actionShot.url}
-              alt={teamMember.firstName}
-              style={{
-                objectFit: "cover",
-              }}
-            />
-          )}
-        </swiper-slide>
-      ))}
-    </swiper-container>
+    <div className="xl:w-xl w-full">
+      <swiper-container init="false">
+        {gcdTeam?.map((teamMember: any) => (
+          <swiper-slide
+            key={teamMember.firstName + teamMember.lastName}
+            class="h-[300px] w-[300px]"
+          >
+            {teamMember.actionShot && (
+              <ContentfulImage
+                priority
+                src={teamMember.actionShot.url}
+                alt={teamMember.firstName}
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+            )}
+          </swiper-slide>
+        ))}
+      </swiper-container>
+    </div>
   );
 }
