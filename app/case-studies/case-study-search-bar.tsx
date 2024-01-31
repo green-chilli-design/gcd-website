4be38fc53816770ad81481ab2b596a385979791c
Rose <rose@greenchillidesign.com>
2024-01-31 13:59:31 +1300

    case study search bar

--- app/case-studies/case-study-search-bar.tsx
+++ app/case-studies/case-study-search-bar.tsx
@@ -1,14 +1,65 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { SubCategoryItem } from "../components/contentful-content-blocks/ServicesSection";
+import { useCallback } from "react";
 
-export default function CaseStudySearchBar() {
+export default function CaseStudySearchBar({
+  categories,
+}: {
+  categories: SubCategoryItem[];
+}) {
   const searchParams = useSearchParams();
+  const category = searchParams.get("category");
+  const router = useRouter();
+  const pathname = usePathname();
 
-  const search = searchParams.get("category");
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams],
+  );
 
-  // This will not be logged on the server when using static rendering
-  console.log(search);
+  function selectCategory(category: string) {
+    if (category !== "All") {
+      router.push(pathname + "?" + createQueryString("category", category));
+    } else {
+      // TODO: this shouldn't remove other search params
+      // Should just delete the category param?
+      router.push(pathname);
+    }
+  }
 
-  return <section className="main-content my-10">Search: {search}</section>;
+  return (
+    <section className="main-content my-10">
+      <div className="mb-5 flex flex-col gap-2">
+        <label>Category:</label>
+        <div className="nowrap flex items-center gap-5 md:max-w-[300px]">
+          <Select onValueChange={(value) => selectCategory(value)}>
+            <SelectTrigger>
+              <SelectValue placeholder={category ? category : "All"} />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value={"All"}>All</SelectItem>
+              {categories.map((category) => (
+                <SelectItem key={category.name} value={category.name}>
+                  {category.name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    </section>
+  );
 }
