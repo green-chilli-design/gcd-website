f97e57b959cd1565f19e223c01c37f19c7b2ae1a
Rose <rose@greenchillidesign.com>
2024-02-16 11:05:07 +1300

    use env var

--- next.config.js
+++ next.config.js
@@ -42,7 +42,6 @@ module.exports = {
   },
 };
 
-
 // Injected content via Sentry wizard below
 
 const { withSentryConfig } = require("@sentry/nextjs");
@@ -62,12 +61,6 @@ module.exports = withSentryConfig(
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: false,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: false,
-
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: "/monitoring",
 
@@ -82,5 +75,5 @@ module.exports = withSentryConfig(
     // https://docs.sentry.io/product/crons/
     // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
-  }
+  },
 );
--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,21 +3,23 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+const sampleRate =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? 0.1 : 1;
 
 Sentry.init({
   dsn: "https://7bf744f90e7ff7249502fee614dc230e@o1036063.ingest.sentry.io/4506669618495488",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.1,
+  tracesSampleRate: sampleRate,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  replaysOnErrorSampleRate: 0.2,
+  replaysOnErrorSampleRate: sampleRate,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: sampleRate,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
