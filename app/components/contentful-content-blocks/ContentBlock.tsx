ba846c7dd758f57783ac7608eb39e65451a8c5f7
Rose <rose@greenchillidesign.com>
2024-02-16 16:52:13 +1300

    add a max width to content so its readable

--- app/components/contentful-content-blocks/ContentBlock.tsx
+++ app/components/contentful-content-blocks/ContentBlock.tsx
@@ -4,13 +4,17 @@ export default function ContentBlock({ contentBlock }: { contentBlock: any }) {
   return (
     <section
       key={contentBlock.heading}
-      className="main-content py-5 text-center lg:py-16"
+      className="main-content flex justify-center py-5 lg:py-16"
     >
-      {contentBlock.heading && <h2 className="mb-5">{contentBlock.heading}</h2>}
-      {contentBlock.subHeading && (
-        <h5 className="mb-2">{contentBlock.subHeading}</h5>
-      )}
-      <Markdown content={contentBlock.contentBody} />
+      <div className="max-w-[846px]">
+        {contentBlock.heading && (
+          <h2 className="mb-5">{contentBlock.heading}</h2>
+        )}
+        {contentBlock.subHeading && (
+          <h5 className="mb-2">{contentBlock.subHeading}</h5>
+        )}
+        <Markdown content={contentBlock.contentBody} />
+      </div>
     </section>
   );
 }
