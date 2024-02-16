cbc8eb93ab980cd9606e0c335b59ddc7ae0d3194
Rose <rose@greenchillidesign.com>
2024-02-16 16:51:58 +1300

    updated footer design

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,9 +14,9 @@ export default function Footer() {
   }
 
   return (
-    <footer className="main-content pb-5 pt-[50px] lg:py-14">
-      <div className="mb-[50px] justify-between lg:mb-14 lg:flex">
-        <div className="mb-[50px] lg:mb-0">
+    <footer className="main-content pb-5 pt-[50px] md:py-14">
+      <div className="mb-[50px] justify-between md:mb-14 md:flex">
+        <div className="mb-[50px] md:mb-0">
           <Link href="/">
             <Image
               src={logoSrc}
@@ -27,10 +27,10 @@ export default function Footer() {
           </Link>
         </div>
 
-        <div className="flex flex-col justify-end lg:flex-row lg:gap-10">
+        <div className="flex flex-col justify-end md:flex-row md:gap-10">
           <Link
             href="/privacy-policy"
-            className="mb-10 text-sm font-bold transition duration-300 hover:text-green lg:mb-0"
+            className="mb-10 text-sm font-bold transition duration-300 hover:text-green md:mb-0"
           >
             Privacy Policy
           </Link>
@@ -43,27 +43,36 @@ export default function Footer() {
         </div>
       </div>
 
-      {/* TODO: Disabled Subscribe form for MVP */}
-      {/* <p>Select topics and stay current with our latest insights</p>
-        <SubscribeForm /> */}
-
-      <div className="flex flex-col-reverse justify-between lg:flex-row lg:flex-wrap lg:items-end">
-        <div className="text-2xs">
+      <div className="flex flex-col justify-between md:flex-row md:flex-wrap md:items-end">
+        <Link
+          href="https://marketplace.govt.nz/"
+          target="_blank"
+          className="mb-[37px] md:mb-0"
+        >
+          <Image
+            width={99}
+            height={31}
+            src={"/images/pae-hokohoko-marketplace.png"}
+            alt="pae hokohoko marketplace logo"
+          />
+        </Link>
+        <div className="order-last text-2xs md:order-none">
           Green Chilli Design · Copyright © {new Date().getFullYear()}
         </div>
-        <div className="mb-[37px] flex flex-row gap-5 lg:mb-0">
-          <a href="https://www.facebook.com/GCDigitalFB" target="_blank">
-            <Image src={"/icons/facebook.svg"} alt="facebook logo" />
-          </a>
-          <a href="https://twitter.com/gcdigital_tweet" target="_blank">
-            <Image src={"/icons/twitter.svg"} alt="twitter logo" />
-          </a>
-          <a
+        <div className="mb-[37px] flex flex-row items-center gap-5 md:mb-0">
+          {/* TODO: Disabled Subscribe form for MVP */}
+          {/* <SubscribeForm /> */}
+          <Link
             href="https://www.linkedin.com/company/green-chilli-digital/"
             target="_blank"
           >
-            <Image src={"/icons/linkedin.svg"} alt="linkedin logo" />
-          </a>
+            <Image
+              width={40}
+              height={40}
+              src={"/icons/linkedin.svg"}
+              alt="linkedin logo"
+            />
+          </Link>
         </div>
       </div>
     </footer>
