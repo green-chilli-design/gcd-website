3d43c383d24e0945b0d1a8ee73bc3803e9fbf04f
Rose <rose@greenchillidesign.com>
2024-03-06 17:39:35 +1300

    hover effect

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -13,19 +13,21 @@ export default function Card({
   return (
     <Link
       href={link}
-      className="relative h-[440px] w-full max-w-[800px] cursor-pointer overflow-hidden rounded-br-[30px] rounded-tl-[30px] bg-black-80 transition duration-500 hover:scale-105"
+      className="relative h-[440px] w-full max-w-[800px] cursor-pointer overflow-hidden rounded-br-[30px] rounded-tl-[30px] bg-black-80"
     >
-      <ContentfulImage
-        priority
-        src={image.url}
-        alt={title}
-        fill={true}
-        style={{
-          objectFit: "cover",
-        }}
-        sizes="(max-width: 320px) 90vw, (max-width: 1024px) 50vw, 33vw"
-      />
-      <div className="absolute bottom-0 h-2/3 w-full bg-gradient-to-t from-black"></div>
+      <div className="absolute h-full w-full transition duration-500 hover:scale-105">
+        <ContentfulImage
+          priority
+          src={image.url}
+          alt={title}
+          fill={true}
+          style={{
+            objectFit: "cover",
+          }}
+          sizes="(max-width: 320px) 90vw, (max-width: 1024px) 50vw, 33vw"
+        />
+        <div className="absolute bottom-0 h-2/3 w-full bg-gradient-to-t from-black"></div>
+      </div>
       <div className="absolute bottom-0 flex w-full items-center justify-between px-5 pb-6 text-neutral">
         <h5>{title}</h5>
         <span className="material-symbols-outlined icon-24 transition duration-500 hover:scale-125">
