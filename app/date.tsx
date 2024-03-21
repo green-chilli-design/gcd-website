f0a365df4fa7e89f41c72914195b44352ad7f97d
Rose <rose@greenchillidesign.com>
2024-03-22 12:33:51 +1300

    use Intl.DateTimeFormat instead of date-fns

--- app/date.tsx
+++ app/date.tsx
@@ -1,9 +1,8 @@
-import { format } from "date-fns";
-
 export default function DateComponent({ dateString }: { dateString: string }) {
-  return (
-    <time dateTime={dateString}>
-      {format(new Date(dateString), "LLLL	d, yyyy")}
-    </time>
-  );
+  const formattedDate = new Intl.DateTimeFormat("en-NZ", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(new Date(dateString));
+  return <time dateTime={dateString}>{formattedDate}</time>;
 }
