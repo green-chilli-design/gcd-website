941e88c2a647a5dc472155f5b057241a86ed6891
Rose <rose@greenchillidesign.com>
2024-03-22 17:42:25 +1300

    pr comments

--- app/components/contentful-content-blocks/ContentBlock.tsx
+++ app/components/contentful-content-blocks/ContentBlock.tsx
@@ -2,10 +2,7 @@ import { Markdown } from "@/lib/markdown";
 
 export default function ContentBlock({ contentBlock }: { contentBlock: any }) {
   return (
-    <section
-      key={contentBlock.heading}
-      className="main-content flex justify-center py-5 lg:py-40"
-    >
+    <section className="main-content flex justify-center py-5 lg:py-40">
       <div className="max-w-[846px] text-center">
         {contentBlock.heading && (
           <h2 className="mb-5">{contentBlock.heading}</h2>
--- app/components/providers/recaptcha-provider.tsx
+++ app/components/providers/recaptcha-provider.tsx
@@ -8,15 +8,14 @@ export default function RecaptchaProvider({
   children: React.ReactNode;
 }) {
   const recaptchaPublicKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "";
+  if (!recaptchaPublicKey) {
+    throw new Error("Recaptcha public key not found");
+  }
 
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={recaptchaPublicKey}
       scriptProps={{ async: true, defer: true }}
-      // container={{
-      //   element: "recaptcha-badge",
-      //   parameters: {},
-      // }}
     >
       {children}
     </GoogleReCaptchaProvider>
