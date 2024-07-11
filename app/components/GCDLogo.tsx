39ff9273131391e84aef13d83efb514f5fff2276
juliuskarl <julius.karl.macrohon@gmail.com>
2024-07-11 21:13:34 +1200

    dynamic gcd logo

---
+++ app/components/GCDLogo.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useTheme } from "next-themes";
+
+interface GCDLogoProps {
+  className?: string;
+}
+
+export default function GCDLogo({ className = "" }: GCDLogoProps) {
+  const { resolvedTheme } = useTheme();
+
+  let logoSrc = "/gcd-logo-round-black.svg";
+  if (resolvedTheme === "dark") {
+    logoSrc = "/gcd-logo-round-white.svg";
+  }
+
+  return (
+    <div className="flex items-center">
+      <Link href="/">
+        <Image
+          src={logoSrc}
+          alt="Green Chilli Design Logo"
+          width="80"
+          height="80"
+          className={"transition duration-500 hover:scale-110 " + className}
+        />
+      </Link>
+    </div>
+  );
+}
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
 import NavMenu from "./NavMenu";
 import NavMenuMobile from "./NavMenuMobile";
+import GCDLogo from "./GCDLogo";
 
 export default function NavBar() {
   const pathname = usePathname();
@@ -19,17 +20,7 @@ export default function NavBar() {
   return (
     <header className="z-10 w-full">
       <nav className="main-content flex items-center justify-between py-5 lg:pb-5 lg:pt-[30px]">
-        <div className="flex items-center">
-          <Link href="/">
-            <Image
-              src={logoSrc}
-              alt="Green Chilli Design Logo"
-              width={80}
-              height={80}
-              className="transition duration-500 hover:scale-110"
-            />
-          </Link>
-        </div>
+        <GCDLogo />
 
         <div>
           <NavMenu />
