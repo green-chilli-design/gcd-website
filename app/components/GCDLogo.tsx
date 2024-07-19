0cdaffe41dbb7b6af57caa15f991f48fb5846a7f
juliuskarl <julius.karl.macrohon@gmail.com>
2024-07-19 20:46:58 +1200

    remove unused imports

--- app/components/GCDLogo.tsx
+++ app/components/GCDLogo.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useTheme } from "next-themes";
-import { cn } from "@/lib/utils";
 
 interface GCDLogoProps {
   className?: string;
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,22 +1,10 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useTheme } from "next-themes";
 import NavMenu from "./NavMenu";
 import NavMenuMobile from "./NavMenuMobile";
 import GCDLogo from "./GCDLogo";
 
 export default function NavBar() {
-  const pathname = usePathname();
-  const { resolvedTheme } = useTheme();
-
-  let logoSrc = "/gcd-logo-round-black.svg";
-  if (resolvedTheme === "dark") {
-    logoSrc = "/gcd-logo-round-white.svg";
-  }
-
   return (
     <header className="z-10 w-full">
       <nav className="main-content flex items-center justify-between py-5 lg:pb-5 lg:pt-[30px]">
